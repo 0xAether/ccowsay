8d10cb26a6fa2fe458e7b5023eaeee6130b7e170
Aaron <clockworkcloudking@gmail.com>
2012-06-22 14:02:44 -0400

    Initial commit

---
+++ cowsay.c
@@ -0,0 +1,57 @@
+/*
+ *
+ * The original cowsay program in C
+ * By: Aaron
+ * June 22, 2012
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void printcow(void);
+
+int main(int argc, char *argv[]) {
+	if( argc == 1 ) {
+		exit(1);
+	}
+	unsigned short counter;
+	unsigned short argscharcount=0;
+	
+	for(counter=1; counter < argc; counter++) {
+		if (counter == ( argc - 1)) {
+			argscharcount=(argscharcount + (strlen(argv[counter])));
+		}
+		else {
+			argscharcount=(argscharcount + (strlen(argv[counter])) + 1);
+		}
+	}
+	argscharcount=argscharcount + 2;
+	
+	printf(" ");
+	for(counter=1; counter <= argscharcount; counter++) {
+		printf("_");
+	}
+	printf("\n< ");
+	for(counter=1; counter < argc; counter++) {
+		if( counter == ( argc - 1 )) {
+			printf("%s", argv[counter]);
+		}
+		else {
+			printf("%s ", argv[counter]);
+		}
+	}
+	printf(" >\n ");
+	for(counter=1; counter <= argscharcount; counter++) {
+		printf("-");
+	}
+	printf("\n");
+	printcow();
+	return 0;
+	
+}
+
+void printcow(void) {
+	printf("        \\   ^__^\n         \\  (oo)\\_______\n            (__)\\       )\\/\\\n                ||----w |\n                ||     ||\n");
+}
